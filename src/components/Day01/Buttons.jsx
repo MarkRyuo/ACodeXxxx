70e468c644fa0fb4a436ba1a09fae968909de6db
MarkRyuo <malupajhonmark3035@gmail.com>
2024-12-22 16:42:52 +0800

    Subject: Update

--- src/components/Day01/Buttons.jsx
+++ src/components/Day01/Buttons.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 
 function Buttons() {
+
+  const [button] = useState ([
+    {id: 1, name: 'YoutubeBtn', color: 'red'},
+    {id: 2, name: 'Button 2', color: 'blue'},
+    {id: 3, name: 'Button 3', color: 'green'},
+    {id: 4, name: 'Button 4', color: 'yellow'},
+  ])
+
   return (
-    <div>
+    <>
       
-    </div>
+    </>
   )
 }
 
