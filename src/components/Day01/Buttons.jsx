6e59508262d0712fdaefdd0fb387c896bc9453a2
MarkRyuo <malupajhonmark3035@gmail.com>
2024-12-22 16:51:28 +0800

    Subject: Update

--- src/components/Day01/Buttons.jsx
+++ src/components/Day01/Buttons.jsx
@@ -3,16 +3,13 @@ import { useState } from "react";
 function Buttons() {
 
   const [button] = useState ([
-    {id: 1, name: 'Subscribe', color: 'red'},
-    {id: 2, name: 'Button 2', color: 'blue'},
-    {id: 3, name: 'Button 3', color: 'green'},
-    {id: 4, name: 'Button 4', color: 'yellow'},
+    {id: 1, name: 'Subscribe', path: ''}
   ])
 
   return (
     <>
       {button.map((btn) => ((
-        <button key={btn.id} type="button">
+        <button key={btn.id} type="button" style>
           {btn.name}
         </button>
       )))}
